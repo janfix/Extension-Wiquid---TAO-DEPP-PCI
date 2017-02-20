2ad3162db18db1cc0720ea09669faeabacf257f7
janfix <jean.philippe.riviere@gmail.com>
2017-02-20 16:30:49 +0100

    delete abeille from Master

--- views/js/pciCreator/dev/abeille/creator/widget/states/states.js
+++
@@ -1,8 +0,0 @@
-define([
-    'taoQtiItem/qtiCreator/widgets/states/factory',
-    'taoQtiItem/qtiCreator/widgets/interactions/customInteraction/states/states',
-    'abeille/creator/widget/states/Question'
-], function(factory, states){
-	
-    return factory.createBundle(states, arguments, ['answer', 'correct', 'map']);
-});
