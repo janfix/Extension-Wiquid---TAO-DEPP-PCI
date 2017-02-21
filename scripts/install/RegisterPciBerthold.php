1711e4d53d810a3d991837e2e58425f2e63517e9
Sam Sipasseuth <sam@taotesting.com>
2017-02-21 12:48:37 +0100

    fixed update script

--- scripts/install/RegisterPciBerthold.php
+++ scripts/install/RegisterPciBerthold.php
@@ -3,14 +3,14 @@
  * DEPP
  */
 
-namespace oat\qtiItemPci\scripts\install;
+namespace oat\pciWiquid\scripts\install;
 
 use oat\taoQtiItem\model\portableElement\action\RegisterPortableElement;
 
 class RegisterPciBerthold extends RegisterPortableElement
 {
     protected function getSourceDirectory(){
-        $viewDir = \common_ext_ExtensionsManager::singleton()->getExtensionById('qtiItemPci')->getConstant('DIR_VIEWS');
+        $viewDir = \common_ext_ExtensionsManager::singleton()->getExtensionById('pciWiquid')->getConstant('DIR_VIEWS');
         return $viewDir.implode(DIRECTORY_SEPARATOR, ['js', 'pciCreator', 'Berthold']);
     }
 }
