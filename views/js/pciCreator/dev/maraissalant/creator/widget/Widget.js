ba57fbd0bd321b1d95f44a6cab80c62c0cda7579
janfix <jean.philippe.riviere@gmail.com>
2017-03-03 11:51:14 +0100

    script install et update et modifications... :->

---
+++ views/js/pciCreator/dev/maraissalant/creator/widget/Widget.js
@@ -0,0 +1,18 @@
+define([
+    'taoQtiItem/qtiCreator/widgets/interactions/customInteraction/Widget',
+    'maraissalant/creator/widget/states/states'
+], function(Widget, states){
+
+	"use strict";
+
+    var MaraissalantInteractionWidget = Widget.clone();
+
+    MaraissalantInteractionWidget.initCreator = function(){
+        
+        this.registerStates(states);
+        
+        Widget.initCreator.call(this);
+    };
+    
+    return MaraissalantInteractionWidget;
+});
