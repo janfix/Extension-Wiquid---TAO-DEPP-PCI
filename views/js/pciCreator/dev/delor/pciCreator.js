493bd9b7fab188aa9e1bcd08cba8dde20506783c
janfix <jean.philippe.riviere@gmail.com>
2017-03-14 20:28:00 +0100

    suppression des id du markup

--- views/js/pciCreator/dev/delor/pciCreator.js
+++ views/js/pciCreator/dev/delor/pciCreator.js
@@ -46,10 +46,8 @@ define([
          * @returns {Object}
          */
         afterCreate : function(pci){
+           //TODO - fix me when more than one instance of such PCIs is implemented 
             $("div .delor").each(function(index, el) {
-
-                //TODO - fix me when more than one instance of such PCIs is implemented
-
                 if(this){
                    $(".widget-box").remove();
                    $(".grid-row").remove();
