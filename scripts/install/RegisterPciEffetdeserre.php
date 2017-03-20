e05fc606a6e979ef5b7be41ff855ba7c6ac2db65
janfix <jean.philippe.riviere@gmail.com>
2017-03-20 18:16:00 +0100

    correction path script install

--- scripts/install/RegisterPciEffetdeserre.php
+++ scripts/install/RegisterPciEffetdeserre.php
@@ -8,6 +8,6 @@ class RegisterPciEffetdeserre extends RegisterPortableElement
 {
     protected function getSourceDirectory(){
         $viewDir = \common_ext_ExtensionsManager::singleton()->getExtensionById('pciWiquid')->getConstant('DIR_VIEWS');
-        return $viewDir.implode(DIRECTORY_SEPARATOR, ['js', 'pciCreator', 'effetdeserre']);  
+        return $viewDir.implode(DIRECTORY_SEPARATOR, ['js', 'pciCreator','dev' ,'effetdeserre']);  
     }
 }
