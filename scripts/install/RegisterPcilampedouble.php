4679ceef34e254048c77e675ab4ee5fa130b179c
janfix <jean.philippe.riviere@gmail.com>
2017-03-20 18:23:58 +0100

    correction path script install

--- scripts/install/RegisterPcilampedouble.php
+++ scripts/install/RegisterPcilampedouble.php
@@ -8,6 +8,6 @@ class RegisterPciLampedouble extends RegisterPortableElement
 {
     protected function getSourceDirectory(){
         $viewDir = \common_ext_ExtensionsManager::singleton()->getExtensionById('pciWiquid')->getConstant('DIR_VIEWS');
-        return $viewDir.implode(DIRECTORY_SEPARATOR, ['js', 'pciCreator', 'lampedouble']);  
+        return $viewDir.implode(DIRECTORY_SEPARATOR, ['js', 'pciCreator', 'dev', 'lampedouble']);  
     }
 }
