951f0135877644a1139f87e1faa55f62930379ba
janfix <jean.philippe.riviere@gmail.com>
2017-03-20 18:26:17 +0100

    correction path script install

--- scripts/install/RegisterPciLentilles.php
+++ scripts/install/RegisterPciLentilles.php
@@ -8,6 +8,6 @@ class RegisterPciLentilles extends RegisterPortableElement
 {
     protected function getSourceDirectory(){
         $viewDir = \common_ext_ExtensionsManager::singleton()->getExtensionById('pciWiquid')->getConstant('DIR_VIEWS');
-        return $viewDir.implode(DIRECTORY_SEPARATOR, ['js', 'pciCreator', 'lentilles']);  
+        return $viewDir.implode(DIRECTORY_SEPARATOR, ['js', 'pciCreator', 'dev', 'lentilles']);  
     }
 }
