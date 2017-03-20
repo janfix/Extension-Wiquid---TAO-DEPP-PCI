a3bb9bc11d8630b275763122f02b9d2786686e1a
janfix <jean.philippe.riviere@gmail.com>
2017-03-20 17:56:31 +0100

    correction path script install

--- scripts/install/RegisterPciBronco.php
+++ scripts/install/RegisterPciBronco.php
@@ -8,6 +8,6 @@ class RegisterPciBronco extends RegisterPortableElement
 {
     protected function getSourceDirectory(){
         $viewDir = \common_ext_ExtensionsManager::singleton()->getExtensionById('pciWiquid')->getConstant('DIR_VIEWS');
-        return $viewDir.implode(DIRECTORY_SEPARATOR, ['js', 'pciCreator', 'bronco']);  
+        return $viewDir.implode(DIRECTORY_SEPARATOR, ['js', 'pciCreator', 'dev' ,'bronco']);  
     }
 }
