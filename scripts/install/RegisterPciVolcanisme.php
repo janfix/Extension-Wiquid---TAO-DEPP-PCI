880a036ccea8a7c7d8ad093c696a51fedc2a4a08
janfix <jean.philippe.riviere@gmail.com>
2017-03-21 08:31:28 +0100

    correction path script install

--- scripts/install/RegisterPciVolcanisme.php
+++ scripts/install/RegisterPciVolcanisme.php
@@ -8,6 +8,6 @@ class RegisterPciVolcanisme extends RegisterPortableElement
 {
     protected function getSourceDirectory(){
         $viewDir = \common_ext_ExtensionsManager::singleton()->getExtensionById('pciWiquid')->getConstant('DIR_VIEWS');
-        return $viewDir.implode(DIRECTORY_SEPARATOR, ['js', 'pciCreator', 'volcanisme']);  
+        return $viewDir.implode(DIRECTORY_SEPARATOR, ['js', 'pciCreator', 'dev' ,'volcanisme']);  
     }
 }
