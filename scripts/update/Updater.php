cb47915d0c60602d083814ec3ad82c3d6e5a2118
janfix <jean.philippe.riviere@gmail.com>
2017-03-28 09:09:04 +0200

    version extention et update

--- scripts/update/Updater.php
+++ scripts/update/Updater.php
@@ -33,7 +33,7 @@ class Updater extends \common_ext_ExtensionUpdater
         $this->setVersion('1.0.0');
 
         if ($this->isVersion('1.0.0')) {
-            call_user_func(new RegisterPciEffetdeserre(), ['0.1.0']);
+            call_user_func(new RegisterPciEffetdeserre(), ['1.1.0']);
             $this->setVersion('1.1.0');
         }
     }
