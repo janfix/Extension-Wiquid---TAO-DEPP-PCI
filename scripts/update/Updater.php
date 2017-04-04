a4d13edaf4ddbb39beeeb50de84ae147f84be13c
janfix <jean.philippe.riviere@gmail.com>
2017-04-04 17:31:46 +0200

    ajout à la suite du script update historique vers 1.13.2

--- scripts/update/Updater.php
+++ scripts/update/Updater.php
@@ -43,6 +43,75 @@ class Updater extends \common_ext_ExtensionUpdater
      */
     public function update($initialVersion)
     {
+
+        $this->setVersion('1.0.0');
+
+        if ($this->isVersion('1.0.0')) {
+            call_user_func(new RegisterPciDelor(), ['1.1.0']);
+            $this->setVersion('1.1.0');
+        }
+
+        if ($this->isVersion('1.1.0')) {
+            call_user_func(new RegisterPciForcegravite(), ['1.1.0']);
+            $this->setVersion('1.2.0');
+        }
+
+        if ($this->isVersion('1.2.0')) {
+            call_user_func(new RegisterPciCuisine(), ['1.1.0']);
+            $this->setVersion('1.3.0');
+        }
+
+        if ($this->isVersion('1.3.0')) {
+            call_user_func(new RegisterPciTrain(), ['1.1.0']);
+            $this->setVersion('1.4.0');
+        }
+
+        if ($this->isVersion('1.4.0')) {
+            call_user_func(new RegisterPciNterre(), ['1.1.0']);
+            $this->setVersion('1.5.0');
+        }
+
+        if ($this->isVersion('1.5.0')) {
+            call_user_func(new RegisterPciMaraissalant(), ['1.1.0']);
+            $this->setVersion('1.6.0');
+        }
+
+        if ($this->isVersion('1.6.0')) {
+            call_user_func(new RegisterPciAbeille(), ['1.1.0']);
+            $this->setVersion('1.7.0');
+        }
+
+        if ($this->isVersion('1.7.0')) {
+            call_user_func(new RegisterPciBronco(), ['1.1.0']);
+            $this->setVersion('1.8.0');
+        }
+
+        if ($this->isVersion('1.8.0')) {
+            call_user_func(new RegisterPciRelatem(), ['1.1.0']);
+            $this->setVersion('1.9.0');
+        }
+
+        if ($this->isVersion('1.9.0')) {
+            call_user_func(new RegisterPciEffetdeserre(), ['1.1.0']);
+            $this->setVersion('1.10.0');
+        }
+
+        if ($this->isVersion('1.10.0')) {
+            call_user_func(new RegisterPciLampedouble(), ['1.1.0']);
+            $this->setVersion('1.11.0');
+        }
+
+        if ($this->isVersion('1.11.0')) {
+            call_user_func(new RegisterPciCircuit(), ['1.1.0']);
+            $this->setVersion('1.12.0');
+        }
+
+        if ($this->isVersion('1.12.0')) {
+            call_user_func(new RegisterPciBerthold (), ['1.1.0']);
+            $this->setVersion('1.13.0');
+        }
+
+        $this->skip('1.13.0', '1.13.1');
         
         if ($this->isVersion('1.13.1')) {
             call_user_func(new RegisterPciDelor(), ['1.1.1']);
