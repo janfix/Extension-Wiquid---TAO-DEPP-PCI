1a7a2955f2e359f21e4497e369ad0fca654077f8
janfix <jean.philippe.riviere@gmail.com>
2018-11-05 15:50:28 +0100

    script register

---
+++ scripts/install/RegisterPciExplo.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Copyright DEPP
+ */
+namespace oat\pciWiquid\scripts\install;
+use oat\taoQtiItem\model\portableElement\action\RegisterPortableElement;
+class RegisterPciExplo extends RegisterPortableElement
+{
+    protected function getSourceDirectory(){
+        $viewDir = \common_ext_ExtensionsManager::singleton()->getExtensionById('pciWiquid')->getConstant('DIR_VIEWS');
+        return $viewDir.implode(DIRECTORY_SEPARATOR, ['js', 'pciCreator','dev' ,'explo']);  
+    }
+}
